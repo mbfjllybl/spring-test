9567c722b9f348033b457f0cd6ac6a9969e9b20b
mbfjllybl <mbfjllybl@163.com>
2021-10-24 10:01:43 +0800

    add some code

--- src/io/github/test/UserTest.java
+++ src/io/github/test/UserTest.java
@@ -2,12 +2,14 @@ package io.github.test;
 
 import io.github.mbfjllybl.User;
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserTest {
     @Test
     public void test() {
+        // BeanFactory
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
         User user = (User)context.getBean("user");
         System.out.println("QAQ");
