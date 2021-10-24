5bc0a2e01d9f11cdd8b0623aa6edb5d4c8b43da6
mbfjllybl <mbfjllybl@163.com>
2021-10-24 10:07:45 +0800

    add some code

--- src/io/github/mbfjllybl/User.java
+++ src/io/github/mbfjllybl/User.java
@@ -1,6 +1,16 @@
 package io.github.mbfjllybl;
 
 public class User {
+    public String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public void add() {
         System.out.println("hello spring");
     }
